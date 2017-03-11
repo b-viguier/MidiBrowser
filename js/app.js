5d24eba4dd4039f229ff9b00103315fee2843056
Benoit Viguier <benoit.viguier@elao.com>
2017-03-11 22:36:43 +0100

    Input/Output selector

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@
         midi: {
             error: null,
             access: null,
-            accuracy: 10 /*ms*/
+            input: null,
+            output: null
         }
     };
 
@@ -15,13 +16,41 @@
             navigator.requestMIDIAccess().then(
                 function (midiAccess) {
                     app.$data.midi.access = midiAccess;
+                    app.$data.midi.access.onstatechange = function () {
+                        app.$forceUpdate();
+                    };
                 },
                 function (msg) {
                     app.$data.midi.error = msg;
                 }
             );
-        }
+        },
+        watch: {
+            "midi.input": function (newInput, oldInput) {
+                if (newInput) {
+                    newInput.onmidimessage = onMidiEvent;
+                }
+                if (oldInput) {
+                    oldInput.onmidimessage = null;
+                }
+            }
+        },
+        methods: {}
     });
 
+    function onMidiEvent(event) {
+        // Mask off the lower nibble (MIDI channel, which we don't care about)
+        switch (event.data[0] & 0xf0) {
+            case 0x90:
+                if (event.data[2] != 0) {  // if velocity != 0, this is a note-on message
+                    console.log("noteOn", event.data[1], event.timeStamp);
+                    return;
+                }
+            // if velocity == 0, fall thru: it's a note-off.
+            case 0x80:
+                console.log("noteOff", event.data[1], event.timeStamp);
+                return;
+        }
+    }
 
 })();
